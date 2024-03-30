53a879a322fc786e801278ba4f478b9450a11bf9
rmtkfka1 <132369391+rmtkfka1@users.noreply.github.com>
2024-03-30 22:17:04 +0900

    index buffer 추가 및 사각형그리기

--- Client/Game.cpp
+++ Client/Game.cpp
@@ -12,16 +12,19 @@ void Game::Init(const WindowInfo& info)
 	_mesh = make_shared<Mesh>();
 	_shader = make_shared<Shader>();
 
-	vector<Vertex> vec(3);
-	vec[0].pos = Vec3(0.f, 0.5f, 0.5f);
+	vector<Vertex> vec(4);
+	vec[0].pos = Vec3(-0.5f, 0.5f, 0.5f);
 	vec[0].color = Vec4(1.f, 0.f, 0.f, 1.f);
-	vec[1].pos = Vec3(0.5f, -0.5f, 0.5f);
-	vec[1].color = Vec4(0.f, 1.0f, 0.f, 1.f);
-	vec[2].pos = Vec3(-0.5f, -0.5f, 0.5f);
+	vec[1].pos = Vec3(0.5f, 0.5f, 0.5f);
+	vec[1].color = Vec4(0.f, 1.f, 0.f, 1.f);
+	vec[2].pos = Vec3(0.5f, -0.5f, 0.5f);
 	vec[2].color = Vec4(0.f, 0.f, 1.f, 1.f);
+	vec[3].pos = Vec3(-0.5f, -0.5f, 0.5f);
+	vec[3].color = Vec4(0.f, 1.f, 0.f, 1.f);
 
+	vector<uint32> index{ 0,1,2,0,2,3 };
 
-	_mesh->Init(vec);
+	_mesh->Init(vec,index);
 	_shader->Init(L"..\\Resource\\Default.hlsl");
 
 }
--- Client/Mesh.cpp
+++ Client/Mesh.cpp
@@ -1,7 +1,24 @@
 #include "pch.h"
 #include "Mesh.h"
 #include "Core.h"
-void Mesh::Init(vector<Vertex>& vec)
+
+
+void Mesh::Init(vector<Vertex>& vec, vector<uint32>& index)
+{
+	CreateVertxBuffer(vec);
+	CreateIndexBuffer(index);
+
+}
+
+void Mesh::Render()
+{
+	CORE->GetCmdList()->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+	CORE->GetCmdList()->IASetVertexBuffers(0, 1, &_vertexBufferView); // Slot: (0~15)
+	CORE->GetCmdList()->IASetIndexBuffer(&_indexBufferView);
+	CORE->GetCmdList()->DrawIndexedInstanced(_indexCount, 1, 0, 0,0);
+}
+
+void Mesh::CreateVertxBuffer(vector<Vertex>& vec)
 {
 	_vertexCount = static_cast<uint32>(vec.size());
 	uint32 bufferSize = _vertexCount * sizeof(Vertex);
@@ -31,9 +48,32 @@ void Mesh::Init(vector<Vertex>& vec)
 
 }
 
-void Mesh::Render()
+void Mesh::CreateIndexBuffer(vector<uint32>& buffer)
 {
-	CORE->GetCmdList()->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
-	CORE->GetCmdList()->IASetVertexBuffers(0, 1, &_vertexBufferView); // Slot: (0~15)
-	CORE->GetCmdList()->DrawInstanced(_vertexCount, 1, 0, 0);
+	_indexCount = static_cast<uint32>(buffer.size());
+	uint32 bufferSize = _indexCount * sizeof(uint32);
+
+	D3D12_HEAP_PROPERTIES heapProperty = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD);
+	D3D12_RESOURCE_DESC desc = CD3DX12_RESOURCE_DESC::Buffer(bufferSize);
+
+	CORE->GetDevice()->CreateCommittedResource(
+		&heapProperty,
+		D3D12_HEAP_FLAG_NONE,
+		&desc,
+		D3D12_RESOURCE_STATE_GENERIC_READ,
+		nullptr,
+		IID_PPV_ARGS(&_indexBuffer));
+	
+	void* indexDataBuffer = nullptr;
+	CD3DX12_RANGE readRange(0, 0); // We do not intend to read from this resource on the CPU.
+	_indexBuffer->Map(0, &readRange, &indexDataBuffer);
+	::memcpy(indexDataBuffer, buffer.data(), bufferSize);
+	_indexBuffer->Unmap(0, nullptr);
+
+	_indexBufferView.BufferLocation = _indexBuffer->GetGPUVirtualAddress();
+	_indexBufferView.Format = DXGI_FORMAT_R32_UINT;
+	_indexBufferView.SizeInBytes = bufferSize;
+
+
 }
+
